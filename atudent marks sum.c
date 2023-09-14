dbc43c0a749727519f969a77ce34f24b7d9add20
Nishant Panchal <120048543+panchal0691@users.noreply.github.com>
2023-09-14 17:32:08 +0530

    Create atudent marks sum.c

---
+++ atudent marks sum.c
@@ -0,0 +1,9 @@
+int marks_summation(int* marks, int number_of_students, char gender) {
+    //Write your code here.
+    int boys = 0, girls = 0;
+    for (int i = 0; i < number_of_students; i++) {
+        if (i % 2) girls += marks[i];
+        else boys += marks[i];
+    }
+    return (gender == 'b') ? boys : girls;
+}
